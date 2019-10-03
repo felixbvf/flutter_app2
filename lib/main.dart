bd21352e0bbb38aaa923675902f048383e6ac494
felixbvf <felixbvf@gmail.com>
2019-10-03 17:58:41 -0400

    Intro app stateful

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:stateful_app2/src/button.dart';
+void main(){
+  runApp(
+    MaterialApp(
+      home: MyButton()
+    )
+  );
+}
---
+++ lib/src/button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget{
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Stateful widget"),
+        )
+    );
+  }
+}
