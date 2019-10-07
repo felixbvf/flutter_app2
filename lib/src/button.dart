7f2b6c8bd79700bee0b4542a4303cf90e9312048
felixbvf <felixbvf@gmail.com>
2019-10-07 16:47:14 -0400

    padding

--- lib/src/button.dart
+++ lib/src/button.dart
@@ -11,7 +11,33 @@ class _MyButtonState extends State<MyButton>{
     return Scaffold(
         appBar: AppBar(
           title: Text("Stateful widget"),
-        )
+          backgroundColor: Colors.orangeAccent,
+        ),
+
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                    "Felix",
+                    style: TextStyle(
+                      fontSize: 40.0
+                    ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0)
+                ),
+                RaisedButton(
+                  child: Text("Actualizar", style: TextStyle(color: Colors.white),),
+                  color: Colors.blueAccent,
+                  onPressed: () {},
+                ),
+              ],
+            )
+          ),
+        ),
+
     );
   }
 }
